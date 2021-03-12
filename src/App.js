93fd9966223f537680097810612bec3d99f57f88
sqnguyen820 <sqnguyen829@gmail.com>
2021-03-12 17:03:46 -0600

    got semantic to work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Container, Header, List } from "semantic-ui-react";
+
+// import pkg from 'semantic-ui-react/package.json'
+// import Example from "./example";
+
+// const App = ({ children }) => (
+//   <Container style={{ margin: 20 }}>
+//     <Example />
+//     <Header as="h3">This example is powered by Semantic UI React {pkg.version} 😊</Header>
+//     <List bulleted>
+//       <List.Item
+//         as="a"
+//         content="💌 Official documentation"
+//         href="https://react.semantic-ui.com/"
+//         target="_blank"
+//       />
+//       <List.Item
+//         as="a"
+//         content="💡 StackOverflow"
+//         href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+//         target="_blank"
+//       />
+//     </List>
+
+//     {children}
+//   </Container>
+// );
+
 import bg from './portfolioBG.jpg';
 import logo from './logo.png';
 // import bg from './bookBG.jpg';
 import './App.css';
+import Nav from './containers/Nav'
 
 const bgStyle = {
   width: '85%'
@@ -10,7 +41,8 @@ const bgStyle = {
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
+      <header className="App-header">
+        <Nav />
         <img src={logo} style={{width:'3%', marginTop:'0px'}}/>
         <p>
           Steven Nguyen
